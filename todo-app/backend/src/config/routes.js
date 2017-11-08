dd3ee975e7af7f90bf98015b7e727206b608bdcc
Rafael Henrique da Silva <rafaelsilva@sl-002864.cwinet.local>
2017-11-08 15:06:21 -0200

    comecar projeto todo backend

--- todo-app/backend/src/config/routes.js
+++ todo-app/backend/src/config/routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 
-module.exports = function(server) {
+module.exports = (server) => {
+  // API ROUTES
+  const router = express.Router()
+  server.use('/api', router)
 
-    // API Routes
-    const router = express.Router()
-    server.use('/api', router)
-
-    // TODO Routes
-    const todoService = require('../api/todo/todoService')
-    todoService.register(router, '/todos')
+  // TODO ROUTES
+  const todoService = require('../api/todo/todoService')
+  todoService.register(router, '/todos')
 }
